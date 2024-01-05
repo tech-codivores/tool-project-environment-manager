1cd58a310948ab27f4d2ba45e4dceb228a549f36
agnonym <contact@agnonym.com>
2024-01-05 17:34:26 +0100

    + | Component - Ui - Alert

---
+++ resources/views/components/ui/alert.blade.php
@@ -0,0 +1,6 @@
+<div class="relative -left-3 w-max bg-red-700">
+    <div class="mx-auto w-screen py-3">
+        <div class="text-center font-semibold text-red-100">{{ $text }}</div>
+        {{ $slot }}
+    </div>
+</div>
