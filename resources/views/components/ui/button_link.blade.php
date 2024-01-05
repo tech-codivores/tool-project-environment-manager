d7764321bd8bc5297912a66aa9be2865157df7ae
agnonym <contact@agnonym.com>
2024-01-05 16:34:16 +0100

    + | Component - Ui - Button Link / Tooltip

---
+++ resources/views/components/ui/button_link.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ $url }}"
+    class="mx-auto my-2 flex w-full items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-700 hover:text-white focus-visible:outline-offset-0">
+    {{ $label }}
+</a>
---
+++ resources/views/components/ui/tooltip.blade.php
@@ -0,0 +1,5 @@
+<div class="has-tooltip">
+    <span
+        class="tooltip pointer-events-none -ml-1 -mt-8 w-max rounded bg-gray-600 px-2 py-1 text-sm text-white shadow">{{ $tooltip }}</span>
+    {{ $slot }}
+</div>
