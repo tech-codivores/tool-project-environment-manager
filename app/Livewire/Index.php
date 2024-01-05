d7ae8cba944c96b874c80c0ab34bab6dad263235
agnonym <contact@agnonym.com>
2024-01-05 17:43:43 +0100

    * | Livewire - Index: use Command DockerRunningCheckAction

--- app/Livewire/Index.php
+++ app/Livewire/Index.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Actions\Command\DockerRunningCheckAction as CommandDockerRunningCheckAction;
 use App\Models\Project;
 use App\Models\State;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public bool $isDockerRunning = false;
+
+    public function mount(CommandDockerRunningCheckAction $commandDockerRunningCheckAction)
+    {
+        $this->isDockerRunning = $commandDockerRunningCheckAction->run();
+    }
+
     public function render()
     {
         $projectRunningList = State::get('running') ?? [];
--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -1,5 +1,9 @@
 <div>
-    <x-project.list :list="$projectList" :runningList="$projectRunningList" />
+    @if (!$isDockerRunning)
+        <x-ui.alert :text="__('Docker is not running')" />
+    @endif
+
+    <x-project.list :list="$projectList" :runningList="$projectRunningList" :isDockerRunning="$isDockerRunning" />
 
     <x-ui.button_link :url="route('project.edit')" :label="__('Add a project')" />
 </div>
